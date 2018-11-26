df2a9d2c0d4248ffe0d240e0c08de139174bab97
Pierrick Coulon <coulonpierrick@gmail.com>
2018-11-26 22:21:43 +0100

    bu ok

---
+++ lab3-bu/App.js
@@ -0,0 +1,136 @@
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View } from 'react-native'
+import { SwitchNavigator } from 'react-navigation'
+// import the different screens
+import Loading from './src/Loading'
+import SignUp from './src/SignUp'
+import Login from './src/Login'
+import Main from './src/Main'
+// create our app's navigation stack
+const App = SwitchNavigator(
+  {
+    Loading,
+    SignUp,
+    Login,
+    Main
+  },
+  {
+    initialRouteName: 'Loading'
+  }
+)
+export default App
+// import React, { Component } from 'react';
+// import { Platform, StyleSheet, Text, View, ImageBackground, Image, TextInput, Dimensions, TouchableOpacity } from 'react-native';
+// import bgImage from './images/background.jpg'
+// import logo from './images/logo.png'
+// import Icon from 'react-native-vector-icons/Ionicons'
+// import SignUp from './SignUp'
+
+// const { width: WIDTH } = Dimensions.get('window')
+
+// type Props = {};
+// export default class App extends Component<Props> {
+
+
+//   render() {
+//     return (
+//       <ImageBackground source={bgImage} style={styles.backgroundContainer}>
+//         <View style={styles.logoContainer}>
+//           <Image source={logo} style={styles.logo} />
+//           <Text style={styles.logoText}>MESSAGING</Text>
+//         </View>
+
+//         <View style={styles.inputContainer}>
+//           <Icon name={'ios-person'} size={28} color={'#CFAFDD'} style={styles.inputIcon} />
+//           <TextInput
+//             style={styles.input}
+//             placeholder={'Username'}
+//             placeholderTextColor={'#CFAFDD'}
+//           />
+//         </View>
+
+//         <View style={styles.inputContainer}>
+//           <Icon name={'ios-lock'} size={28} color={'#CFAFDD'} style={styles.inputIcon} />
+//           <TextInput
+//             style={styles.input}
+//             placeholder={'Password'}
+//             secureTextEntry={true}
+//             placeholderTextColor={'#CFAFDD'}
+//           />
+//         </View>
+
+//         <TouchableOpacity style={styles.btnLogin}>
+//           <Text style={styles.text}>Login</Text>
+//         </TouchableOpacity>
+
+//         <TouchableOpacity style={styles.btnLogin}>
+//           <Text style={styles.text}>Sign Up</Text>
+//         </TouchableOpacity>
+//       </ImageBackground>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   backgroundContainer: {
+//     flex: 1,
+//     width: null,
+//     height: null,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+
+//   logo: {
+//     width: 100,
+//     height: 100,
+//   },
+
+//   logoContainer: {
+//     alignItems: 'center',
+//     marginBottom: 50,
+//   },
+
+//   logoText: {
+//     color: 'white',
+//     fontSize: 25,
+//     fontWeight: '500',
+//     marginTop: 10,
+//     opacity: 0.7,
+//   },
+
+//   input: {
+//     width: WIDTH - 55,
+//     height: 45,
+//     borderRadius: 25,
+//     fontSize: 16,
+//     paddingLeft: 45,
+//     backgroundColor: 'rgba(0,0,0, 0.35)',
+//     color: 'rgba(255,255,255,0.7)',
+//     marginHorizontal: 25,
+//   },
+
+//   inputIcon: {
+//     position: 'absolute',
+//     top: 8,
+//     left: 37
+//   },
+
+//   inputContainer: {
+//     marginTop: 10,
+//   },
+
+//   btnLogin: {
+//     width: WIDTH - 55,
+//     height: 45,
+//     borderRadius: 25,
+//     backgroundColor: '#CFAFDD',
+//     justifyContent: 'center',
+//     marginTop: 20
+//   },
+
+//   text: {
+//     color: 'rgba(255, 255, 255, 1)',
+//     fontSize: 16,
+//     textAlign: 'center'
+//   }
+// });
--- lab3/App.js
+++ lab3/App.js
@@ -1,136 +1,14 @@
-import React from 'react'
-import { StyleSheet, Platform, Image, Text, View } from 'react-native'
-import { SwitchNavigator } from 'react-navigation'
-// import the different screens
-import Loading from './Loading'
-import SignUp from './SignUp'
-import Login from './Login'
-import Main from './Main'
-// create our app's navigation stack
-const App = SwitchNavigator(
-  {
-    Loading,
-    SignUp,
-    Login,
-    Main
-  },
-  {
-    initialRouteName: 'Loading'
-  }
-)
-export default App
-// import React, { Component } from 'react';
-// import { Platform, StyleSheet, Text, View, ImageBackground, Image, TextInput, Dimensions, TouchableOpacity } from 'react-native';
-// import bgImage from './images/background.jpg'
-// import logo from './images/logo.png'
-// import Icon from 'react-native-vector-icons/Ionicons'
-// import SignUp from './SignUp'
+// Import the screens
+import Main from './components/Main';
+import Chat from './components/Chat';
+// Import React Navigation
+import { createStackNavigator } from 'react-navigation'
 
-// const { width: WIDTH } = Dimensions.get('window')
+// Create the navigator
+const navigator = createStackNavigator({
+  Main: { screen: Main },
+  Chat: { screen: Chat },
+});
 
-// type Props = {};
-// export default class App extends Component<Props> {
-
-
-//   render() {
-//     return (
-//       <ImageBackground source={bgImage} style={styles.backgroundContainer}>
-//         <View style={styles.logoContainer}>
-//           <Image source={logo} style={styles.logo} />
-//           <Text style={styles.logoText}>MESSAGING</Text>
-//         </View>
-
-//         <View style={styles.inputContainer}>
-//           <Icon name={'ios-person'} size={28} color={'#CFAFDD'} style={styles.inputIcon} />
-//           <TextInput
-//             style={styles.input}
-//             placeholder={'Username'}
-//             placeholderTextColor={'#CFAFDD'}
-//           />
-//         </View>
-
-//         <View style={styles.inputContainer}>
-//           <Icon name={'ios-lock'} size={28} color={'#CFAFDD'} style={styles.inputIcon} />
-//           <TextInput
-//             style={styles.input}
-//             placeholder={'Password'}
-//             secureTextEntry={true}
-//             placeholderTextColor={'#CFAFDD'}
-//           />
-//         </View>
-
-//         <TouchableOpacity style={styles.btnLogin}>
-//           <Text style={styles.text}>Login</Text>
-//         </TouchableOpacity>
-
-//         <TouchableOpacity style={styles.btnLogin}>
-//           <Text style={styles.text}>Sign Up</Text>
-//         </TouchableOpacity>
-//       </ImageBackground>
-//     );
-//   }
-// }
-
-// const styles = StyleSheet.create({
-//   backgroundContainer: {
-//     flex: 1,
-//     width: null,
-//     height: null,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//   },
-
-//   logo: {
-//     width: 100,
-//     height: 100,
-//   },
-
-//   logoContainer: {
-//     alignItems: 'center',
-//     marginBottom: 50,
-//   },
-
-//   logoText: {
-//     color: 'white',
-//     fontSize: 25,
-//     fontWeight: '500',
-//     marginTop: 10,
-//     opacity: 0.7,
-//   },
-
-//   input: {
-//     width: WIDTH - 55,
-//     height: 45,
-//     borderRadius: 25,
-//     fontSize: 16,
-//     paddingLeft: 45,
-//     backgroundColor: 'rgba(0,0,0, 0.35)',
-//     color: 'rgba(255,255,255,0.7)',
-//     marginHorizontal: 25,
-//   },
-
-//   inputIcon: {
-//     position: 'absolute',
-//     top: 8,
-//     left: 37
-//   },
-
-//   inputContainer: {
-//     marginTop: 10,
-//   },
-
-//   btnLogin: {
-//     width: WIDTH - 55,
-//     height: 45,
-//     borderRadius: 25,
-//     backgroundColor: '#CFAFDD',
-//     justifyContent: 'center',
-//     marginTop: 20
-//   },
-
-//   text: {
-//     color: 'rgba(255, 255, 255, 1)',
-//     fontSize: 16,
-//     textAlign: 'center'
-//   }
-// });
+// Export it as the root component
+export default navigator
